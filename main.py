c24d183ea185a98b15496dab299e2781f14a8747
= <=>
2025-01-15 18:53:51 +0800

    Added get_char_dict function to main.py

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ path_to_file = "books/frankenstein.txt"
 def main():
     text = getBookText(path_to_file)
     word_count = getWordCount(text)
-    print(f"{word_count} words found in book.")
     
+    char_dict = get_char_dict(text)
+    print(char_dict)
+
 
 def getBookText(path_to_file):
      with open(path_to_file) as f:
@@ -13,5 +15,17 @@ def getBookText(path_to_file):
 def getWordCount(text):
     count = len(text.split())
     return count
-    
+
+
+def get_char_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+        
+
 main()
