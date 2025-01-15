c33c9624a7bcda462a6c76a3d4a787f7e3fd33ca
= <=>
2025-01-15 19:38:24 +0800

    Added book_report, sort_on and sort_char_dict functions in main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 path_to_file = "books/frankenstein.txt"
+test_char_list = [{"char":"a","count":"100"},{"char":"b","count":"200"},{"char":"c","count":"300"},{"char":"d","count":"400"},{"char":",","count":"100"}]
 
 def main():
     text = getBookText(path_to_file)
     word_count = getWordCount(text)
     
     char_dict = get_char_dict(text)
-    print(char_dict)
+    sorted_char_list = sort_char_dict(char_dict)
+    
+    book_report(word_count,sorted_char_list)
 
 
 def getBookText(path_to_file):
@@ -26,6 +29,27 @@ def get_char_dict(text):
         else:
             chars[lowered] = 1
     return chars
-        
 
+def sort_on(dict):
+    return dict["count"]
+
+def sort_char_dict(char_dict):
+    sorted_list =[]
+    for ch in char_dict:
+        sorted_list.append({"char": ch, "count": char_dict[ch]})
+    sorted_list.sort(reverse= True, key= sort_on)
+    return sorted_list
+    
+    
+def book_report(word_count,char_list):
+    print(f"--- Begin report of {path_to_file} ---")
+    print(f"{word_count} words found in the document\n")
+    
+    for c in char_list:
+        if not c["char"].isalpha():
+            continue
+        print(f"The '{c["char"]}' character was found {c["count"]} times.")
+
+    print("--- End report ---")
+    
 main()
