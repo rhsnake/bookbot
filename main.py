100ce3af61652da0154bc960c7cc5a20a38a8faf
= <=>
2025-01-15 18:17:21 +0800

    Added getWordCount function to main.py

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ path_to_file = "books/frankenstein.txt"
 
 def main():
     text = getBookText(path_to_file)
-    print(text)
+    word_count = getWordCount(text)
+    print(f"{word_count} words found in book.")
     
 
 def getBookText(path_to_file):
      with open(path_to_file) as f:
                 return f.read()
 
-
+def getWordCount(text):
+    count = len(text.split())
+    return count
+    
 main()
